17cd4c9746a5236b2cf5831a73ba8e684259702d
Mikiyas Getachew <mikiyasgetachew77@gmail.com>
2023-07-17 10:10:30 +0530

    ADD: done method to mark the item as completed RETRIVE and set fuctionality for cli commands

--- javascript/task.js
+++ javascript/task.js
@@ -1,3 +1,4 @@
+// task.js
 
 const fs = require('fs');
 
@@ -29,3 +30,37 @@ function add(priority, taskDetail) {
     console.error('Error:', error);
   }
 }
+
+function done(priority){
+    try {
+        const fileData = fs.readFileSync('task.txt', 'utf8');
+        tasks = JSON.parse(fileData);
+      } catch (error) {
+        console.log("Creating new file ...")
+      }
+
+      let task = tasks.find(task => task.priority === priority); // find the tasks
+      task.completed = true;
+
+      try {
+        const incompletedTasks = tasks.filter(task => task.priority !== priority); //filter the completed task
+        fs.writeFileSync('task.txt', JSON.stringify(incompletedTasks, null, 2));
+        console.log(`Task completed sucessful`);
+        fs.appendFileSync('complete.txt', JSON.stringify(task, null, 2));
+      } catch (error) {
+        console.error('Error:', error);
+      }
+}
+
+
+const command = process.argv[2]; // function
+const priority = process.argv[3]; // priority
+const taskDetail = process.argv[4];
+
+if( command == 'add'){
+  add(priority, taskDetail)
+}
+else if( command == 'done'){
+  done(priority)
+}
+
